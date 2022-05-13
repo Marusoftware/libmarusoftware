9ab62430045b9e6debfef33ad3a4c14b4f591d24
Maruo.S <raspi-maru2004@outlook.jp>
2022-05-13 12:01:05 +0900

    delete addon feature

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from .data import Config, Lang
-from .addon import Addon
 from . import core, exception, ui as UI
 
-__all__ = ["Config", "Lang", "core", "exception", "UI", "Addon"]
+__all__ = ["Config", "Lang", "core", "exception", "UI"]
